5ffebca50b0623d4ed2262e3956e8d8de282a3e2
Zarif <zarifraja19@gmail.com>
2023-04-09 02:01:05 +0600

    added

--- src/Components/BusinessMan/BusinessMan.jsx
+++ src/Components/BusinessMan/BusinessMan.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const BusinessMan = ({data}) => {
-    let {name} =data;
+    let {name,phone,email} =data;
     return (
-        <div>
+        <div className='border py-10 rounded-xl border-black'>
             <p>{name}</p>
+            <p>{phone}</p>
+            <p>{email}</p>
+            <Link className='hover:border-green-400 border border-black mt-10 py-2 px-4 rounded-xl inline-block'>Show details</Link>
         </div>
     );
 };
--- src/Components/BusinessMen/BusinessMen.jsx
+++ src/Components/BusinessMen/BusinessMen.jsx
@@ -7,13 +7,17 @@ const BusinessMen = () => {
     // console.log(posts);
     return (
         <div>
-            <h1 className='text-2xl mt-10'>Here shows the Business-Men status</h1>
-           <div>
+            <h1 className='text-2xl mt-20'>Here shows the Business-Men status</h1>
+            <hr className='pt-1 bg-slate-500 mt-8 rounded-2xl' />
+           <div className='grid grid-cols-3 gap-20 mt-24'>
             {
                 posts.map(data => <BusinessMan 
                 key={data.id}
                 data={data}
                 ></BusinessMan>)
+            }
+            {
+                posts.map(data => console.log(data))
             }
            </div>
         </div>
